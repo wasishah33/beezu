723195d76b520650b109ea5094ea1348d0bef1ef
Wasif Waheed <wasishah33@gmail.com>
2025-09-19 19:30:14 +0500

    added file upload function in helper.

--- core/helpers.php
+++ core/helpers.php
@@ -140,3 +140,43 @@ function auth_id(): ?int
 {
     return Session::get('user_id');
 }
+
+/**
+ * Handle file upload
+ *
+ * @param string $fieldName The name of the file input field
+ * @param string $folder The folder to upload the file to (relative to public/)
+ * @param array $allowedExtensions List of allowed file extensions
+ * @return string|null The relative path to the uploaded file or null if no file was uploaded
+ * @throws \Exception If the upload fails or the file type is not allowed
+ */
+function upload_file(string $fieldName, string $folder = 'uploads', array $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp']): ?string
+{
+    if (!isset($_FILES[$fieldName]) || $_FILES[$fieldName]['error'] !== UPLOAD_ERR_OK) {
+        return null; // no file uploaded
+    }
+
+    $uploadDir = __DIR__ . "/../public/{$folder}/";
+    $relativePath = "/{$folder}/";
+
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
+    }
+
+    $fileTmpPath = $_FILES[$fieldName]['tmp_name'];
+    $fileName = basename($_FILES[$fieldName]['name']);
+    $extension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+    if (!in_array($extension, $allowedExtensions)) {
+        throw new \Exception("Invalid file type. Allowed: " . implode(', ', $allowedExtensions));
+    }
+
+    $newFileName = uniqid(pathinfo($fileName, PATHINFO_FILENAME) . '_', true) . '.' . $extension;
+    $destination = $uploadDir . $newFileName;
+
+    if (move_uploaded_file($fileTmpPath, $destination)) {
+        return $relativePath . $newFileName;
+    }
+
+    throw new \Exception("Failed to upload file.");
+}
